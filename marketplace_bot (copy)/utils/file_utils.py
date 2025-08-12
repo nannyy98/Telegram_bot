c190aea2b28007af728a5181ffbc47ed564c8c4f
nannyy98 <83236945+nannyy98@users.noreply.github.com>
2025-08-13 01:50:34 +0500

    Delete marketplace_bot (copy) directory

--- marketplace_bot (copy)/utils/file_utils.py
+++
@@ -1,8 +0,0 @@
-import os
-from config import IMAGE_FOLDER
-
-def get_image_path(filename):
-    path = os.path.join(IMAGE_FOLDER, filename)
-    if os.path.exists(path):
-        return path
-    return None
